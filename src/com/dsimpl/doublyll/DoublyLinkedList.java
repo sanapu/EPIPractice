c1cba3826625a5b11a8ba62a22e0899d002ec705
sravanisanapureddy <sravani.sanapureddy@gmail.com>
2020-02-06 00:02:33 -0800

    Epi - DoubleLinkedList -add First last and index, Remove first ast and index, get size

--- src/com/dsimpl/doublyll/DoublyLinkedList.java
+++ src/com/dsimpl/doublyll/DoublyLinkedList.java
@@ -2,6 +2,7 @@ package com.dsimpl.doublyll;
 
 public class DoublyLinkedList {
     Node headNode;
+    int size = 0;
     public void add(int data){
         Node currentNode = new Node(data);
 
@@ -13,9 +14,115 @@ public class DoublyLinkedList {
                 n=n.next;
             }
             n.next = currentNode;
-            currentNode.previous = n.next;
+            currentNode.previous = n;
         }
+        ++size;
 
+    }
+
+    public void addFirst(int data){
+        Node currentNode = new Node(data);
+
+        if(headNode == null){
+            headNode = currentNode;
+        }else{
+            Node n = headNode;
+
+            headNode = currentNode;
+            currentNode.next = n;
+            n.previous = currentNode;
+        }
+        ++size;
+
+    }
+
+    public void removeFirst(){
+        Node nodeToBeDeleted = headNode;
+
+        if(nodeToBeDeleted == null){
+        }else{
+            Node n = headNode.next;
+
+            n.previous= null;
+            headNode.next =null;
+            headNode = n;
+
+        }
+        --size;
+
+    }
+    public void removeLast(){
+        Node nodeToBeDeleted = getNodeAtIndex(getSize()-1);
+
+        if(nodeToBeDeleted == null){
+        }else{
+            Node prevNode = nodeToBeDeleted.previous;
+
+            prevNode.next = null;
+            nodeToBeDeleted.previous =null;
+
+        }
+        --size;
+
+    }
+    public void removeAtIndex(int index){
+        Node nodeToBeDeleted = getNodeAtIndex(index);
+
+        if(nodeToBeDeleted == null){
+        }else{
+            Node prevNode = nodeToBeDeleted.previous;
+
+            prevNode.next = nodeToBeDeleted.next;
+            nodeToBeDeleted.next.previous = prevNode;
+            nodeToBeDeleted.previous =null;
+            nodeToBeDeleted.next =null;
+
+        }
+        --size;
+
+    }
+
+
+
+    public void addAtIndex(int index, int data){
+        Node getNode = getNodeAtIndex(index);
+        System.out.println("getnode data"+getNode.data);
+
+        Node newNode= new Node(data);
+
+        if(getNode != null){
+            Node previousNode = getNode.previous;
+            newNode.next=getNode;
+            newNode.previous = previousNode;
+            getNode.previous = newNode;
+            previousNode.next=newNode;
+
+
+        }
+        ++size;
+
+    }
+
+    protected  Node getNodeAtIndex(int index){
+        Node currentRefNode = headNode;
+        if(currentRefNode == null){
+           return null;
+        }else{
+            int i =0;
+
+            while(i<index && currentRefNode.next != null){
+
+                currentRefNode = currentRefNode.next;
+                ++i;
+            }
+        }
+        return currentRefNode;
+
+
+    }
+
+    public int getSize(){
+        return size;
     }
 
     public String toString(){
@@ -25,7 +132,7 @@ public class DoublyLinkedList {
             s = s+" > "+n.data;
             n = n.next;
         }
-        s = s+" > "+n.data;
+        s = s+" > "+n.data+" last previous"+n.previous.data;
 
 
         return s;
--- src/com/dsimpl/doublyll/DoublyLinkedListTest.java
+++ src/com/dsimpl/doublyll/DoublyLinkedListTest.java
@@ -14,8 +14,31 @@ class DoublyLinkedListTest {
         list.add(2);
         list.add(6);
         list.add(3);
+        list.add(39);
         list.add(9);
         System.out.println("list : "+list.toString());
+
+        list.addFirst(1);
+        System.out.println("list : "+list.toString());
+        System.out.println("list size addfirst: "+list.getSize());
+
+        list.addAtIndex(1,99);
+        System.out.println("list size:add at index "+list.getSize());
+        list.add(100);
+        System.out.println("list : "+list.toString());
+
+        list.removeFirst();
+        System.out.println("list : "+list.toString());
+        System.out.println("list size: after remove at first "+list.getSize());
+
+        list.removeLast();
+        System.out.println("list : "+list.toString());
+        System.out.println("list size: after remove at last "+list.getSize());
+
+        list.removeAtIndex(2);
+        System.out.println("list : "+list.toString());
+        System.out.println("list size: after remove at index 2  "+list.getSize());
+
     }
 
 }
